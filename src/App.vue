2f765d479dc7fc032a0fdde63ed2786c15a43b6c
Ariel Perez <vanciniap@gmail.com>
2022-12-21 16:17:53 -0400

    Add getUserInfo

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
     <p>
       {{ date }}
     </p>
+    <input type="text" v-model="name" @keyup="getUserInfo" />
+    <p v-if="grettings">
+      {{ grettings }}
+    </p>
   </div>
 </template>
 
@@ -17,12 +21,35 @@ export default {
     return {
       message: "Welcome to Lolocar",
       date: new Date(),
+      name: "",
+      grettings: null,
+      timer: null,
     };
   },
   created() {
     setInterval(() => {
       this.date = new Date();
     }, 1000);
+  },
+  methods: {
+    async getUserInfo() {
+      clearTimeout(this.timer);
+
+      this.timer = setTimeout(async () => {
+        this.grettings = null;
+
+        if (this.name.length <= 3) {
+          return;
+        }
+
+        const res = await fetch(`https://api.agify.io/?name=${this.name}`);
+        const result = await res.json();
+
+        this.grettings = `Hola, ${result.name}. Tu nombre tiene ${
+          result.age || 0
+        } años.`;
+      }, 300);
+    },
   },
 };
 </script>
